c8a42dfb5a50616424c3cc666f3a7b402c21f3db
Manish <sdt.manishbailwal@gmail.com>
2025-12-06 11:55:52 +0530

    changed versions

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
-import withPWA from "next-pwa";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-
-  // 👇 Required for Next.js 16 when using any webpack-based plugin (like next-pwa)
-  turbopack: {},        // provide empty config to satisfy Next.js
-  webpack: (config) => {
-    return config;      // this enables webpack mode
-  }
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: false,
-})(nextConfig);
+export default nextConfig;
